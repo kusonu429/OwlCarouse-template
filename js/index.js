893ca2d4749c0386cb2fa39a4cd2c57c4f0ee077
Sonu <129176762+sonuwebdev98@users.noreply.github.com>
2023-05-23 10:05:14 +0530

    update data

---
+++ js/index.js
@@ -0,0 +1,59 @@
+
+
+$('.owl-carousel-one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
+
+
+$('.owl-carousel-two').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+
+
+
+$('.owl-carousel-three').owlCarousel({
+    loop:true,
+    margin:10, 
+    nav:true,
+    responsive:{ 
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
